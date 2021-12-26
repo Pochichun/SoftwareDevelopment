5a5285c190d793e0bf8b62a465211aa76f791969
Pochichun <80267395+Pochichun@users.noreply.github.com>
2021-12-26 20:02:53 +0400

    Add files via upload

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace aaaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dict<string, string> persons = new Dict<string, string>();
+            persons.Add("Олег", "Директор");
+            persons.Add("Не Олег", "Не директор");
+            persons.Add("Мария", "Женщина");
+            persons.Add("Эркюль", "Детектив");
+            persons.Add("Меченый", "Стрелок");
+            persons.Add("Лара", "Крофт");
+            persons.Add("Геральт", "Ведьмак");
+            persons.Add("Мышь", "Кродется");
+            Console.WriteLine("Элементы: ");
+            foreach (var item in persons)
+                Console.WriteLine("{0} {1}", item.Key, item.Value);
+            Console.WriteLine("Количество = {0}", persons.Count);
+            Console.WriteLine();
+            persons["Мария"] = "Кродется";
+            persons["Мышь"] = "Женщина";
+            Console.WriteLine("После смены местами Женщина и Кродется:");
+            foreach (string key in persons.Keys)
+                Console.WriteLine("{0} {1}", key, persons[key]);
+            Console.WriteLine();
+            persons.Remove("Олег");
+            Console.WriteLine("После удаления Олега: ");
+            foreach (string key in persons.Keys)
+                Console.WriteLine("{0} {1}", key, persons[key]);
+            Console.WriteLine();
+            Console.WriteLine("Присутствует ли Олег? ");
+            if (persons.ContainsKey("Олег") == false) {
+                Console.WriteLine("Нет Олега");
+            }
+            else { Console.WriteLine("Есть Олег"); }
+            Console.WriteLine();
+            persons.TryGetValue("Лара", out string value);
+            Console.WriteLine();
+            Console.WriteLine("TryGetValue Лара: {0}", value);
+            Console.WriteLine();
+            KeyValuePair<string, string>[] array = new KeyValuePair<string, string>[9];
+            persons.CopyTo(array, 1);
+            Console.WriteLine("Копировать Бонд Джеймс Бонд");
+            Console.WriteLine();
+            KeyValuePair<string, string> first = new KeyValuePair<string, string>("Бонд", "Джеймс Бонд");
+            array[0] = first;
+            Console.WriteLine("CopyTo :");
+            foreach (var item in array)
+                Console.WriteLine("{0} {1}", item.Key, item.Value);
+          
+        }
+    }
+}
---
+++ Dictionary/Dictionary/dict.cs
@@ -0,0 +1,220 @@
+﻿
+using System.Collections;
+
+
+namespace aaaa
+{
+    public class Dict<TK, TV> : IDictionary<TK, TV>
+    {
+        public ICollection<TK> Keys { get; }
+        public ICollection<TV> Values { get; }
+
+        private LinkedList<KeyValuePair<TK, TV>>[] hashtable;
+     
+        private int count = 0;
+        public int Count => count;
+        public bool IsReadOnly { get; }
+
+
+        public Dict()
+        {
+            Keys = new List<TK>();
+            Values = new List<TV>();
+            IsReadOnly = false;
+            hashtable = new LinkedList<KeyValuePair<TK, TV>>[16];
+            for (int i = 0; i < hashtable.Length; i++)
+                hashtable[i] = new LinkedList<KeyValuePair<TK, TV>>();
+        }
+       
+
+
+        public void Add(TK key, TV value)
+        {
+            int hashTa = Math.Abs(key.GetHashCode()) % hashtable.Length;
+            if (!Keys.Contains(key))
+            {
+                KeyValuePair<TK, TV> pairEl = new KeyValuePair<TK, TV>(key, value);
+                hashtable[hashTa].AddLast(pairEl);
+                count++;
+                Keys.Add(key);
+                Values.Add(value);
+
+                if (count >= hashtable.Length)
+                {
+                    extendD();
+                }
+            }
+            else
+            {
+                throw new ArgumentException("key already exists");
+            }
+        }
+
+
+        private void extendD()
+        {
+            LinkedList<KeyValuePair<TK, TV>>[] hashtable1 = hashtable;
+            hashtable = new LinkedList<KeyValuePair<TK, TV>>[hashtable1.Length * 2];
+
+            foreach (var linkedList in hashtable1)
+            {
+                if (linkedList.Count != 0)
+                    foreach (var item in linkedList)
+                        Add(item);
+            }
+        }
+
+
+        public bool Remove(TK key)
+        {
+            int hashT = Math.Abs(key.GetHashCode()) % hashtable.Length;
+            if (ContainsKey(key))
+            {
+                foreach (var item in hashtable[hashT])
+                {
+                    if (key.Equals(item.Key))
+                    {
+                        count--;
+                        hashtable[hashT].Remove(item);
+                        Keys.Remove(key);
+                        Values.Remove(item.Value);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+
+        public void CopyTo(KeyValuePair<TK, TV>[] array, int arrayIndex)
+        {
+            if (arrayIndex < 0)
+                throw new ArgumentOutOfRangeException("Index < 0");
+            if (array == null)
+                throw new ArgumentNullException("Array = null");
+            if (count > array.Length - 1 - arrayIndex)
+                throw new ArgumentException("Too many elements" );
+            for (int i = 0; i < hashtable.Length; ++i)
+            {
+                foreach (var item in hashtable[i])
+                {
+                    if (arrayIndex >= array.Length) break;
+                    array[arrayIndex] = item;
+                    arrayIndex++;
+                }
+            }
+        }
+
+
+        public bool TryGetValue(TK key, out TV value)
+        {
+            int hashT = Math.Abs(key.GetHashCode()) % hashtable.Length;
+            if (ContainsKey(key))
+            {
+                foreach (var item in hashtable[hashT])
+                {
+                    value = item.Value;
+                    return true;
+                }
+            }
+            value = default;
+            return false;
+        }
+
+
+        public void Clear()
+        {
+            for (int i = 0; i < hashtable.Length; i++)
+                hashtable[i].Clear();
+            Keys.Clear();
+            Values.Clear();
+            count = 0;
+        }
+
+
+        public bool ContainsKey(TK key)
+        {
+            return Keys.Contains(key);
+        }
+
+
+        public bool Contains(KeyValuePair<TK, TV> Pair)
+        {
+            int hashT = Math.Abs(Pair.Key.GetHashCode()) % hashtable.Length;
+            return hashtable[hashT].Contains(Pair);
+        }
+
+
+        public void Add(KeyValuePair<TK, TV> Pair)
+        {
+            Add(Pair.Key, Pair.Value);
+        }
+
+        
+
+        public bool Remove(KeyValuePair<TK, TV> keyValuePair)
+        {
+            return Remove(keyValuePair.Key);
+        }
+       
+        
+        public TV this[TK key]
+        {
+            get
+            {
+                int hashT = Math.Abs(key.GetHashCode()) % hashtable.Length;
+
+                if (Keys.Contains(key))
+                {
+
+                    foreach (var item in hashtable[hashT])
+                    {
+                        if (key.Equals(item.Key))
+                        {
+                            return item.Value;
+                        }
+                    }
+                    return default;
+                }
+                else
+                {
+                    throw new KeyNotFoundException("Not found");
+                }
+            }
+            set
+            {
+                int hashT = Math.Abs(key.GetHashCode()) % hashtable.Length;
+                if (Keys.Contains(key))
+                {
+                    KeyValuePair<TK, TV> newPair = new KeyValuePair<TK, TV>(key, value);
+                    foreach (var item in hashtable[hashT])
+                    {
+                        if (key.Equals(item.Key))
+                        {
+                            hashtable[hashT].Remove(item);
+                            hashtable[hashT].AddLast(newPair);
+                            break;
+                        }
+                    }
+                }
+                else Add(key, value);
+
+            }
+        }
+        public IEnumerator<KeyValuePair<TK, TV>> GetEnumerator()
+        {
+            for (int i = 0; i < hashtable.Length; ++i)
+            {
+                foreach (var item in hashtable[i])
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
